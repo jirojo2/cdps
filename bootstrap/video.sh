8287e68178e8d0bc62028f7b5f303033a789baaf
José Ignacio Rojo <jirojo2@gmail.com>
2014-12-20 19:34:08 +0100

    Ahora se monta el glusterfs como sudo en los servidores de video

--- bootstrap/video.sh
+++ bootstrap/video.sh
@@ -4,9 +4,9 @@
 git clone cdps@10.1.1.2:p7/dev/.git
 
 # mount nas
-mkdir -p /mnt/nas
-mount -t glusterfs 10.1.3.21:/nas -o backup-volfile-servers=10.1.3.22:10.1.3.23 /mnt/nas
-chmod 777 /mnt/nas
+sudo mkdir -p /mnt/nas
+sudo mount -t glusterfs 10.1.3.21:/nas -o backup-volfile-servers=10.1.3.22:10.1.3.23 /mnt/nas
+sudo chmod 777 /mnt/nas
 
 # launch nodejs 
 forever start -c "bash" ~/dev/run.sh nas
