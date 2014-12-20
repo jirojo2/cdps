ce15f30e139dc4b6030b666438b0071218ad07b6
José Ignacio Rojo <jirojo2@gmail.com>
2014-12-20 19:25:37 +0100

    cambiado el modo de lanzar nodejs

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ "$1" == "www" ]; then
+	npm install
+	bower install
+	cd backend && node server.js
+fi
+
+if [ "$1" == "nas" ]; then
+	npm install
+	cd nas && node server.js
+fi
