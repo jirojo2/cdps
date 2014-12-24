305d88e05eee2b8f67fb7c107a5f3ed93d312fd8
José Ignacio Rojo <jirojo2@gmail.com>
2014-12-24 15:16:49 +0100

    wip upload

--- frontend/modules/videos/upload.js
+++ frontend/modules/videos/upload.js
@@ -26,29 +26,34 @@ angular.module('cdps.videos.upload', ['ngRoute'])
     		$scope.video = { allocating: true };
     		$scope.error = null;
 			$scope.phase = 1;
-
-    		Videos.allocateVideo(file.name ,function(err, id) {
-    			if (err) {
-    				return $scope.error = "Error al subir el archivo de vídeo";
-    			}
-
-				$scope.phase = 2;
-    			$scope.video.id = id;
-    			$scope.video.allocating = false;
-    			$scope.video.loading = true;
-
-    			Videos.upload(file, id, function(err) {
-                    $scope.phase = 3;
-                    $scope.video.loading = false;
-                    $scope.video.uploaded = true;
-
-                    console.log(err||'video subido correctamente ' + id);
-                });
-    		});
+    		
     	} 
     	else {
     		$scope.error = "Archivo de vídeo incorrecto";
     	}
+    }
+
+    $scope.uploadVideo = function() {
+        $scope.phase = 2;
+
+        Videos.allocateVideo(file.name ,function(err, id) {
+            if (err) {
+                return $scope.error = "Error al subir el archivo de vídeo";
+            }
+
+            $scope.phase = 3;
+            $scope.video.id = id;
+            $scope.video.allocating = false;
+            $scope.video.loading = true;
+
+            Videos.upload(file, id, function(err) {
+                $scope.phase = 4;
+                $scope.video.loading = false;
+                $scope.video.uploaded = true;
+
+                console.log(err||'video subido correctamente ' + id);
+            });
+        });
     }
 
 }]);
