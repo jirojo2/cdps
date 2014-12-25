51268c993868e9ca510e5ad8ba01f2f4d181fac4
José Ignacio Rojo <jirojo2@gmail.com>
2014-12-25 02:02:13 +0100

    npm install silenciado

--- run.sh
+++ run.sh
@@ -3,12 +3,12 @@
 cd ~/dev/
 
 if [ "$1" == "www" ]; then
-	npm install > /dev/null
-	bower install > /dev/null
+	npm install &>/dev/null
+	bower install &>/dev/null
 	cd backend && forever start server.js
 fi
 
 if [ "$1" == "nas" ]; then
-	npm install > /dev/null
+	npm install &>/dev/null
 	cd nas && forever start server.js
 fi
