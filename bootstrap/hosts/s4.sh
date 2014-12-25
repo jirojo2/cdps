8603d87120e5c9f12acf629b0f188591ebd71b85
José Ignacio Rojo <jirojo2@gmail.com>
2014-12-25 02:07:35 +0100

    fix bootstrap: la replica se añade una vez se han arrancado todos los mongod involucrados

--- bootstrap/hosts/s4.sh
+++ bootstrap/hosts/s4.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 ssh ubuntu@s4 'bash -s' < bootstrap/www.sh
-ssh ubuntu@s4 'mongo ~/dev/mongo-replicaset-cdps.js'
--- bootstrap/hosts/s5.sh
+++ bootstrap/hosts/s5.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 ssh ubuntu@s5 'bash -s' < bootstrap/www.sh
+
+# Añadir la réplica en s5, una vez mongod está arrancado
+ssh ubuntu@s4 'mongo ~/dev/mongo-replicaset-cdps.js'
